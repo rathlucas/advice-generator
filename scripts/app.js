84410bec053b94c865a6c8bdf18b79e733d822e2
Lucin <lucin189@gmail.com>
2022-03-14 21:15:40 -0300

    feat(showAdvice): Create showAdvice function.

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+const adviceTitle = document.getElementById("advice-title")
+const adviceText = document.getElementById("advice-text");
+const url = "https://api.adviceslip.com/advice";
+
+const getAdvice = async () => {
+    let response = await fetch(url);
+    let data = await response.json();
+
+    return data.slip;
+}
+
+async function showAdvice() {
+    let slip = await getAdvice();
+
+    adviceTitle.textContent = `ADVICE #${slip.id}`;
+    adviceText.textContent = `${slip.advice}`;
+
+}
+
+showAdvice();
